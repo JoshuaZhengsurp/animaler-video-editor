8fd5c9a8e5df8b90ffa1e734d361c44197faa48e
zhengjiaxin05 <zhengjiaxin05@kuaishou.com>
2025-01-14 13:41:30 +0800

    feat: 🚀 update

--- src/page/Editor/Video/Player/index.tsx
+++ src/page/Editor/Video/Player/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 
 /**
  * @todo 通过canvas绘制播放视频
@@ -9,6 +9,28 @@ interface Iprops {
 }
 
 export default function VideoPlayer(props: Iprops) {
-    console.log('playerWHRate', props.playerWHRate);
-    return <div className='w-full h-full'>player</div>;
+    const { playerWHRate } = props;
+    const playerRef = useRef<HTMLDivElement>(null);
+
+    const getCanvasPlayerWH = useCallback(() => {
+        const [width, height] = playerWHRate;
+        const rate = width / height;
+        const playerWidth = playerRef.current?.clientWidth;
+        const playerHeight = playerRef.current?.clientHeight;
+        if (playerWidth && playerHeight) {
+            if (rate >= playerWidth / playerHeight) {
+            }
+        }
+        return null;
+    }, [playerWHRate, playerRef]);
+
+    useEffect(() => {
+        console.log(playerRef.current?.clientHeight, playerRef.current?.clientWidth);
+    }, []);
+
+    return (
+        <div className='w-full h-full' ref={playerRef}>
+            player
+        </div>
+    );
 }
